3a99530ae376169930736c792551e5381955f3c3
Choongkyu Kim <choongkyu.kim+gh@gmail.com>
2022-02-26 23:25:23 -0800

    remove yarn.lock and update babel and webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,29 @@
-const {join} = require('path');
+const { join } = require('path');
 
 module.exports = {
 
-    context: __dirname,
+  context: __dirname,
 
-    entry: './demo/demo.js',
+  entry: './demo/demo.js',
 
-    output: {
-        path: join(__dirname, '/assets/'),
-        publicPath: '/assets/',
-        filename: './bundle.js',
-    },
+  output: {
+    path: join(__dirname, '/assets/'),
+    publicPath: '/assets/',
+    filename: './bundle.js',
+  },
+  module: {
+    rules: [{
+      test: /\.js$/,
+      exclude: /node_modules/,
+      use: {
+        loader: 'babel-loader',
+      }
+    }]
+  },
 
-    module: {
-        loaders: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                loader: 'babel-loader',
-            }
-        ]
-    },
+  devtool: 'source-map',
 
-    devtool: 'source-map',
-
-    devServer: {
-        port: process.env.PORT || 8080,
-    },
+  devServer: {
+    port: process.env.PORT || 8080,
+  },
 };
